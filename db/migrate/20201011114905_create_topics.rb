e73e16a517816df28798745f6258aac42aa2f944
ting682 <tchung682@gmail.com>
2020-10-12 13:11:37 -0400

    added topics

---
+++ db/migrate/20201011114905_create_topics.rb
@@ -0,0 +1,11 @@
+class CreateTopics < ActiveRecord::Migration[6.0]
+  def change
+    create_table :topics do |t|
+      t.string  :title
+      t.text    :content
+      
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_09_173610) do
+ActiveRecord::Schema.define(version: 2020_10_11_114905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email_address"
