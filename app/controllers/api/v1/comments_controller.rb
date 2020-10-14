71fd7e80d591a9a30b4a86d79b8ca813e5d12a55
ting682 <tchung682@gmail.com>
2020-10-14 10:00:54 -0400

    added show, create, update, and destroy comments and passages

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -7,5 +7,43 @@ class Api::V1::CommentsController < ApplicationController
         render json: comments
     end
 
+    def show
+        comment = Comment.find(params[:id])
+        render json: comment
+    end
 
+    def create
+        comment = Comment.new(comment_params)
+        if comment.valid?
+            comment.save
+            render :json => comment, status: :accepted
+        else
+            #error message
+            render :json => {errors: comment.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+    def update
+        comment = Comment.find(params[:id])
+        if comment.update(comment_params)
+            render :json => comment, status: :accepted
+        else
+            render :json => {errors: comment.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+    def destroy
+        comment = Comment.find(params[:id])
+        if comment.destroy
+            render :json => Comment.all, status: :accepted
+        else 
+            render :json => {errors: comment.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:content, :user_id, :topic_id)
+    end 
 end
--- app/controllers/api/v1/passages_controller.rb
+++ app/controllers/api/v1/passages_controller.rb
@@ -7,5 +7,43 @@ class Api::V1::PassagesController < ApplicationController
         render :json => passages
     end
 
-    
+    def show
+        passage = Passage.find([:id])
+        render :json => passage
+    end
+
+    def create
+        passage = Passage.new(passage_params)
+        if passage.valid?
+            passage.save
+            render :json => passage, status: :accepted
+        else
+            #error message
+            render :json => {errors: passage.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+    def update
+        passage = Passage.find(params[:id])
+        if passage.update(passage_params)
+            render :json => passage, status: :accepted
+        else
+            render :json => {errors: passage.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+    def destroy
+        passage = Passage.find(params[:id])
+        if passage.destroy
+            render :json => Passage.all, status: :accepted
+        else 
+            render :json => {errors: passage.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+    private
+
+    def passage_params
+        params.require(:passage).permit(:content, :book, :chapter, :verse, :user_id)
+    end
 end
