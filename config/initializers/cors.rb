0212e072dc31c324feb9f57983ea8728dbe529a2
ting682 <tchung682@gmail.com>
2020-11-16 11:39:46 -0500

    modified cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://jolly-khorana-0814fc.netlify.app'
 
     resource '*',
       headers: :any,
