e12cbcd2bc4525e88d95156cf0ed05b96f7e745d
ting682 <tchung682@gmail.com>
2020-11-19 19:11:02 -0500

    changed cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://jolly-khorana-0814fc.netlify.app'
+    origins 'https://smallgroups.netlify.app'
     # origins 'https://ting682.github.io/smallgroup-frontend/'
 
 
